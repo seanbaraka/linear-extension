657eecddf948d4e7c51873a84e568d7075532b42
seanbaraka <seanbaraka@gmail.com>
2024-03-18 20:16:10 +0300

    attempts to fix the infinite redirect bug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,25 @@
-import { Navigate, createLazyFileRoute } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useAuth } from "../hooks/userAuth";
 
 function Index() {
   // First check if we have an active session,
   const { session } = useAuth();
+
   // If we don't have a session recorded, prompt the user to login
   if (!session) {
     // navigate({ to: "/login" }).catch((e) => console.log("Fuck", e.message));
-    return <Navigate to="/login" />;
+    // return null;
+    // return <Navigate to="/login" />;
+    return <div className="text-red-500">Not Logged In</div>;
+  } else {
+    return (
+      <>
+        <h1>You are logged in {session.user.email}</h1>
+      </>
+    );
   }
-
-  return (
-    <>
-      <h1>You hare logged in</h1>
-    </>
-  );
 }
 
-export const Route = createLazyFileRoute("/")({
+export const Route = createFileRoute("/")({
   component: Index,
 });
