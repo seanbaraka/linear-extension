6d6ab35d648ae42e90bb8e5447a140988f86665b
seanbaraka <seanbaraka@gmail.com>
2024-04-08 04:52:37 +0300

    adds loading states to the hooks

--- src/hooks/linear.ts
+++ src/hooks/linear.ts
@@ -18,17 +18,20 @@ export enum IssueState {
 
 export const useGetIssues = (status?: IssueState) => {
   const [activeIssues, setActiveIssues] = useState<Issue[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
+    if (!isLoading) return;
     client1
       .issues({
         filter: { state: { name: { contains: status } } },
       })
       .then((data) => {
+        setIsLoading(false);
         setActiveIssues(data.nodes);
       });
-  }, [activeIssues, status]);
-  return activeIssues;
+  }, [activeIssues, isLoading, status]);
+  return { activeIssues, isLoading };
 };
 
 export const useLinearUser = () => {
@@ -42,13 +45,36 @@ export const useLinearUser = () => {
   return user;
 };
 
+export const useGetUserAssignedIssues = () => {
+  const [issues, setIssues] = useState<Issue[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (!isLoading) return;
+    client1.viewer.then((user) => {
+      client1
+        .issues({
+          filter: { assignee: { id: { eq: user.id } } },
+        })
+        .then((data) => {
+          setIssues(data.nodes);
+          setIsLoading(false);
+        });
+    });
+  }, [issues, isLoading]);
+  return { issues, isLoading };
+};
+
 export const useLinearOrg = () => {
   const [organization, setOrganization] = useState<Organization>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   useEffect(() => {
+    if (!isLoading) return;
     client1.organization.then((organization) => {
+      console.log("Organization found,", organization);
+      setIsLoading(false);
       setOrganization(organization);
     });
-  }, [organization]);
-  return organization;
+  }, [organization, isLoading]);
+  return { organization, isLoading };
 };
-console.log("linear");
